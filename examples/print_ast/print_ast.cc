8a72baba2edc619f5b717cb0310b83fdce0a768a
Alex Torres <alexst07@gmail.com>
2016-09-27 23:31:14 -0300

    print ast example

---
+++ examples/print_ast/print_ast.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+#include "parser/parser.h"
+#include "ast/ast-printer.h"
+
+int main(int argc, char **argv) {
+  using namespace setti::internal;
+
+  if (argc < 2) {
+    std::cout << "usage: print_ast <file>\n";
+    return -1;
+  }
+
+  std::ifstream file(argv[1]);
+  std::stringstream buffer;
+  buffer << file.rdbuf();
+
+  Lexer l(buffer.str());
+  std::cout << "Lexer\n";
+  TokenStream ts = l.Scanner();
+  Parser p(std::move(ts));
+  auto res = p.AstGen();
+
+  if (p.nerrors() == 0) {
+    std::cout << "Correct analysis\n";
+    AstPrinter visitor;
+    visitor.Visit(res.NodePtr());
+  } else {
+    std::cout << "Parser error analysis\n";
+  }
+}
