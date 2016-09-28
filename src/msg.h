60056baa3d92be66bbf9f751468bc146698cb3eb
Alex Torres <alexst07@gmail.com>
2016-09-28 18:42:07 -0300

    Implements stream for messages

--- src/msg.h
+++ src/msg.h
@@ -25,6 +25,9 @@ class Message {
       , line_(line)
       , pos_(pos) {}
 
+  friend std::ostream& operator<<(std::ostream& stream, const Message& msg);
+  friend std::ostream& operator<<(std::ostream& stream, Message& msg);
+
  private:
   uint line_;
   uint pos_;
@@ -32,12 +35,75 @@ class Message {
   Severity severity_;
 };
 
+inline std::ostream& operator<<(std::ostream& stream, const Message& msg) {
+  std::string severity;
+
+  switch (msg.severity_) {
+    case Message::Severity::INFO:
+      severity = "info";
+      break;
+
+    case Message::Severity::WARNING:
+      severity = "warning";
+      break;
+
+    case Message::Severity::ERR:
+      severity = "info";
+      break;
+  }
+
+  stream << msg.line_ << ":" << msg.pos_ << ": " << severity << ": " << msg.msg_ << '\n';
+
+  return stream;
+}
+
+inline std::ostream& operator<<(std::ostream& stream, Message& msg) {
+  std::string severity;
+
+  switch (msg.severity_) {
+    case Message::Severity::INFO:
+      severity = "info";
+      break;
+
+    case Message::Severity::WARNING:
+      severity = "warning";
+      break;
+
+    case Message::Severity::ERR:
+      severity = "info";
+      break;
+  }
+
+  stream << msg.line_ << ":" << msg.pos_ << ": " << severity << ": " << msg.msg_ << '\n';
+
+  return stream;
+}
+
 class Messages {
  public:
+  using iterator = std::vector<Message>::iterator;
+  using const_iterator = std::vector<Message>::const_iterator;
+
   Messages() = default;
 
   void Push(Message&& msg) {
     msg_vec_.push_back(std::move(msg));
+  }
+
+  iterator begin() {
+    return msg_vec_.begin();
+  }
+
+  iterator end() {
+    return msg_vec_.end();
+  }
+
+  const_iterator begin() const {
+    return msg_vec_.begin();
+  }
+
+  const_iterator end() const {
+    return msg_vec_.end();
   }
 
  private:
