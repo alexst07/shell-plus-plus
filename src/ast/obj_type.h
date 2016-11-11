cf909be384fdd0c635811c8679f76a82dd57387b
Alex Torres <alexst07@gmail.com>
2016-11-11 19:51:15 -0200

    Implements slice object

--- src/ast/obj_type.h
+++ src/ast/obj_type.h
@@ -40,6 +40,7 @@ class Object: public EntryPointer {
     BOOL,
     REAL,
     STRING,
+    SLICE,
     ARRAY,
     MAP,
     TUPLE,
@@ -245,6 +246,64 @@ class StringObject: public Object {
  private:
   std::string value_;
 };
+
+class SliceObject: public Object {
+ public:
+  SliceObject(ObjectPtr obj_start, ObjectPtr obj_end)
+      : Object(ObjectType::SLICE) {
+    if (obj_start->type() != ObjectType::INT ||
+        obj_end->type() != ObjectType::INT) {
+      throw RunTimeError(RunTimeError::ErrorCode::INCOMPATIBLE_TYPE,
+                         boost::format("slice parameter must be integer"));
+
+      IntObject& int_start = static_cast<IntObject&>(*obj_start);
+      IntObject& int_end = static_cast<IntObject&>(*obj_end);
+
+      start_ = int_start.value();
+      end_ = int_end.value();
+    }
+  }
+
+  SliceObject(const SliceObject& obj)
+      : Object(obj), start_(obj.start_), end_(obj.end_) {}
+
+  virtual ~SliceObject() {}
+
+  SliceObject& operator=(const SliceObject& obj) {
+    start_ = obj.start_;
+    end_ = obj.end_;
+    return *this;
+  }
+
+  inline int start() const noexcept { return start_; }
+  inline int end() const noexcept { return start_; }
+
+  std::size_t Hash() const override {
+    throw RunTimeError(RunTimeError::ErrorCode::INCOMPATIBLE_TYPE,
+                       boost::format("slice object has no hash method"));
+  }
+
+  bool operator==(const Object& obj) const override {
+    if (obj.type() != ObjectType::SLICE) {
+      return false;
+    }
+
+    const SliceObject& slice = static_cast<const SliceObject&>(obj);
+
+    bool exp = (start_ == slice.start_) && (end_ == slice.end_);
+
+    return exp;
+  }
+
+  void Print() override {
+    std::cout << "SLICE: start = " << start_ << ", end = " << end_;
+  }
+
+ private:
+  int start_;
+  int end_;
+};
+
 class TupleObject: public Object {
  public:
    TupleObject(std::vector<std::unique_ptr<Object>>&& value)
