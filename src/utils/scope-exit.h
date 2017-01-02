21d7b18509e5b416c562939aff508a10b6d053d0
Alex Torres <alexst07@gmail.com>
2017-01-02 18:50:08 -0200

    scope exit

---
+++ src/utils/scope-exit.h
@@ -0,0 +1,30 @@
+#ifndef SETI_SCOPE_EXIT_H
+#define SETI_SCOPE_EXIT_H
+
+namespace setti {
+namespace internal {
+
+template <typename T>
+class ScopeExit {
+ public:
+  ScopeExit(T &&f) : f_{std::move(f)} {}
+
+  ~ScopeExit() { f_(); }
+
+ private:
+  T f_;
+};
+
+template <typename T>
+ScopeExit<T> MakeScopeExit(T &&f) {
+  return ScopeExit<T>{std::move(f)};
+}
+
+template <typename T>
+void IgnoreUnused (T const &) {}
+
+}
+}
+
+#endif  // SETI_SCOPE_EXIT_H
+
