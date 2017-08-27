7f66a60a627b382b91e2f9a904a732f70ad12d6d
Alex Torres <alexst07@gmail.com>
2017-08-27 14:20:18 -0300

    separate the symbol table file

---
+++ src/interpreter/symbol-table.cc
@@ -0,0 +1,224 @@
+// Copyright 2016 Alex Silva Torres
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "symbol-table.h"
+
+namespace shpp {
+namespace internal {
+
+SymbolAttr& SymbolTable::SetValue(const std::string& name) {
+  auto it = map_.find(name);
+  if (it != map_.end()) {
+    return it->second;
+  } else {
+    it = map_.begin();
+
+    // declare a variable as local
+    SymbolAttr symbol;
+    SymbolIterator it_symbol = map_.insert (it, std::move(
+        std::pair<std::string, SymbolAttr>(name, std::move(symbol))));
+    return it_symbol->second;
+  }
+}
+
+void SymbolTable::SetValue(const std::string& name,
+    std::shared_ptr<Object> value) {
+  auto it = map_.find(name);
+  if (it != map_.end()) {
+    it->second.set_value(value);
+    return;
+  }
+
+  // declare variable always as local
+  SymbolAttr symbol(value, false);
+  it = map_.begin();
+  map_.insert (it, std::move(std::pair<std::string, SymbolAttr>(
+      name, std::move(symbol))));
+}
+
+bool SymbolTable::SetValue(const std::string& name, SymbolAttr&& symbol) {
+  // if the key exists only change the value
+  auto it = map_.find(name);
+  if (it != map_.end()) {
+    return false;
+  }
+
+  // if the key not exists create a new
+  // max efficiency inserting assign begin to i
+  it = map_.begin();
+  map_.insert(it, std::move(std::pair<std::string, SymbolAttr>(
+      name, std::move(symbol))));
+
+  return true;
+}
+
+SymbolAttr& SymbolTableStack::Lookup(const std::string& name, bool create) {
+  for (int i = (stack_.size() - 1); i >= 0 ; i--) {
+    auto it_obj = stack_.at(i)->Lookup(name);
+
+    if (it_obj != stack_.at(i)->end()) {
+      return it_obj->second;
+    }
+  }
+
+  // search on main table if no symbol was found
+  auto it_obj = main_table_.lock()->Lookup(name);
+
+  if (it_obj != main_table_.lock()->end()) {
+    return it_obj->second;
+  }
+
+  if (create) {
+    if (stack_.size() > 0) {
+      SymbolAttr& ref = stack_.back()->SetValue(name);
+      return ref;
+    } else {
+      SymbolAttr& ref = main_table_.lock()->SetValue(name);
+      return ref;
+    }
+  }
+
+  throw RunTimeError(RunTimeError::ErrorCode::SYMBOL_NOT_FOUND,
+                      boost::format("symbol %1% not found")% name);
+}
+
+bool SymbolTableStack::Exists(const std::string& name) {
+  for (int i = (stack_.size() - 1); i >= 0 ; i--) {
+    auto it_obj = stack_.at(i)->Lookup(name);
+
+    if (it_obj != stack_.at(i)->end()) {
+      return true;
+    }
+  }
+
+  // search on main table if no symbol was found
+  auto it_obj = main_table_.lock()->Lookup(name);
+
+  if (it_obj != main_table_.lock()->end()) {
+    return true;
+  }
+
+  return false;
+}
+
+std::tuple<std::shared_ptr<Object>,bool>
+SymbolTableStack::LookupObj(const std::string& name) {
+  for (int i = (stack_.size() - 1); i >= 0 ; i--) {
+    auto it_obj = stack_.at(i)->Lookup(name);
+
+    if (it_obj != stack_.at(i)->end()) {
+      return std::tuple<std::shared_ptr<Object>,bool>(
+            it_obj->second.SharedAccess(), true);
+    }
+  }
+
+  // search on main table if no symbol was found
+  auto it_obj = main_table_.lock()->Lookup(name);
+
+  if (it_obj != main_table_.lock()->end()) {
+    if (it_obj->second.global()) {
+    return std::tuple<std::shared_ptr<Object>,bool>(
+          it_obj->second.SharedAccess(), true);
+    }
+  }
+
+  return std::tuple<std::shared_ptr<Object>,bool>(
+        std::shared_ptr<Object>(nullptr), false);
+}
+
+bool SymbolTableStack::Remove(const std::string& name) {
+  for (int i = (stack_.size() - 1); i >= 0 ; i--) {
+    bool r = stack_.at(i)->Remove(name);
+
+    if (r) {
+      return r;
+    }
+  }
+
+  return main_table_.lock()->Remove(name);
+}
+
+std::vector<SymbolTablePtr> SymbolTableStack::GetUntilFuncTable() const {
+  std::vector<SymbolTablePtr> statck;
+
+  for (auto& table: stack_) {
+    if (table->Type() != SymbolTable::TableType::FUNC_TABLE) {
+      statck.push_back(table);
+      continue;
+    }
+
+    statck.push_back(table);
+
+    break;
+  }
+
+  return statck;
+}
+
+std::vector<SymbolTablePtr> SymbolTableStack::GetUntilClassTable() const {
+  std::vector<SymbolTablePtr> statck;
+
+  for (auto& table: stack_) {
+    if (table->Type() != SymbolTable::TableType::CLASS_TABLE) {
+      statck.push_back(table);
+      continue;
+    }
+
+    statck.push_back(table);
+
+    break;
+  }
+
+  return statck;
+}
+
+SymbolAttr& SymbolTableStack::LookupClass(const std::string& name) {
+  // search on main table if no symbol was found
+  auto it_obj = class_table_->Lookup(name);
+
+  if (it_obj != class_table_->end()) {
+    return it_obj->second;
+  }
+
+  SymbolAttr& ref = class_table_->SetValue(name);
+  return ref;
+
+  throw RunTimeError(RunTimeError::ErrorCode::SYMBOL_NOT_FOUND,
+                      boost::format("symbol '%1%' not found")% name);
+}
+
+SymbolTablePtr& SymbolTableStack::GetClassTable() {
+  for (auto& table: stack_) {
+    if (table->Type() == SymbolTable::TableType::CLASS_TABLE) {
+      return table;
+    }
+  }
+
+  throw RunTimeError(RunTimeError::ErrorCode::SYMBOL_NOT_FOUND,
+                      boost::format("can't find class symbol table"));
+}
+
+bool SymbolTableStack::ExistsSymbolInClass(const std::string& name) {
+  // search on main table if no symbol was found
+  auto it_obj = class_table_->Lookup(name);
+
+  if (it_obj != class_table_->end()) {
+    return true;
+  }
+
+  return false;
+}
+
+}
+}
--- src/interpreter/symbol-table.h
+++ src/interpreter/symbol-table.h
@@ -74,11 +74,11 @@ class SymbolAttr {
     return *this;
   }
 
-  std::shared_ptr<Object>& Ref() noexcept {
+  inline std::shared_ptr<Object>& Ref() noexcept {
     return value_;
   }
 
-  std::shared_ptr<Object> SharedAccess() const noexcept {
+  inline std::shared_ptr<Object> SharedAccess() const noexcept {
     return value_;
   }
 
@@ -111,7 +111,8 @@ class SymbolTable {
   using SymbolIterator = SymbolMap::iterator;
   using CmdIterator = CmdMap::iterator;
   using SymbolConstIterator = SymbolMap::const_iterator;
-  using CmdAliasMap = std::unordered_map<std::string, std::vector<std::string>>;
+  using CmdAliasMap = std::unordered_map<std::string,
+      std::vector<std::string>>;
 
   SymbolTable(TableType type = TableType::SCOPE_TABLE): type_(type) {}
 
@@ -126,52 +127,13 @@ class SymbolTable {
 
   // Return a reference for symbol if it exists or create a new
   // and return the reference
-  SymbolAttr& SetValue(const std::string& name) {
-    auto it = map_.find(name);
-    if (it != map_.end()) {
-      return it->second;
-    } else {
-      it = map_.begin();
+  SymbolAttr& SetValue(const std::string& name);
 
-      // declare a variable as local
-      SymbolAttr symbol;
-      SymbolIterator it_symbol = map_.insert (it, std::move(
-          std::pair<std::string, SymbolAttr>(name, std::move(symbol))));
-      return it_symbol->second;
-    }
-  }
+  void SetValue(const std::string& name, std::shared_ptr<Object> value);
 
-  void SetValue(const std::string& name, std::shared_ptr<Object> value) {
-    auto it = map_.find(name);
-    if (it != map_.end()) {
-      it->second.set_value(value);
-      return;
-    }
+  bool SetValue(const std::string& name, SymbolAttr&& symbol);
 
-    // declare variable always as local
-    SymbolAttr symbol(value, false);
-    it = map_.begin();
-    map_.insert (it, std::move(std::pair<std::string, SymbolAttr>(
-        name, std::move(symbol))));
-  }
-
-  bool SetValue(const std::string& name, SymbolAttr&& symbol) {
-    // if the key exists only change the value
-    auto it = map_.find(name);
-    if (it != map_.end()) {
-      return false;
-    }
-
-    // if the key not exists create a new
-    // max efficiency inserting assign begin to i
-    it = map_.begin();
-    map_.insert(it, std::move(std::pair<std::string, SymbolAttr>(
-        name, std::move(symbol))));
-
-    return true;
-  }
-
-  void SetCmd(const std::string& name, std::shared_ptr<CmdEntry> cmd) {
+  inline void SetCmd(const std::string& name, std::shared_ptr<CmdEntry> cmd) {
     cmd_map_[name] = cmd;
   }
 
@@ -195,11 +157,12 @@ class SymbolTable {
 
   // insert alias for command, this must be global even if declared
   // inside an if or loop, and in the import must me copied
-  void SetCmdAlias(const std::string& name, std::vector<std::string>&& cmd) {
+  inline void SetCmdAlias(const std::string& name,
+      std::vector<std::string>&& cmd) {
     cmd_alias_[name] = std::move(cmd);
   }
 
-  bool ExistsCmdAlias(const std::string& name) const {
+  inline bool ExistsCmdAlias(const std::string& name) const {
     auto it = cmd_alias_.find(name);
 
     if (it == cmd_alias_.end()) {
@@ -350,91 +313,14 @@ class SymbolTableStack: public SymbolTableStackBase {
   // Search in all stack an return the refence for the symbol if
   // it exists, or if create = true, create a new symbol if it
   // doesn't exists and return its reference
-  SymbolAttr& Lookup(const std::string& name, bool create) override {
-    for (int i = (stack_.size() - 1); i >= 0 ; i--) {
-      auto it_obj = stack_.at(i)->Lookup(name);
+  SymbolAttr& Lookup(const std::string& name, bool create) override;
 
-      if (it_obj != stack_.at(i)->end()) {
-        return it_obj->second;
-      }
-    }
-
-    // search on main table if no symbol was found
-    auto it_obj = main_table_.lock()->Lookup(name);
-
-    if (it_obj != main_table_.lock()->end()) {
-      return it_obj->second;
-    }
-
-    if (create) {
-      if (stack_.size() > 0) {
-        SymbolAttr& ref = stack_.back()->SetValue(name);
-        return ref;
-      } else {
-        SymbolAttr& ref = main_table_.lock()->SetValue(name);
-        return ref;
-      }
-    }
-
-    throw RunTimeError(RunTimeError::ErrorCode::SYMBOL_NOT_FOUND,
-                       boost::format("symbol %1% not found")% name);
-  }
-
-  bool Exists(const std::string& name) {
-    for (int i = (stack_.size() - 1); i >= 0 ; i--) {
-      auto it_obj = stack_.at(i)->Lookup(name);
-
-      if (it_obj != stack_.at(i)->end()) {
-        return true;
-      }
-    }
-
-    // search on main table if no symbol was found
-    auto it_obj = main_table_.lock()->Lookup(name);
-
-    if (it_obj != main_table_.lock()->end()) {
-      return true;
-    }
-
-    return false;
-  }
+  bool Exists(const std::string& name);
 
   std::tuple<std::shared_ptr<Object>,bool>
-  LookupObj(const std::string& name) override {
-    for (int i = (stack_.size() - 1); i >= 0 ; i--) {
-      auto it_obj = stack_.at(i)->Lookup(name);
+  LookupObj(const std::string& name) override;
 
-      if (it_obj != stack_.at(i)->end()) {
-        return std::tuple<std::shared_ptr<Object>,bool>(
-              it_obj->second.SharedAccess(), true);
-      }
-    }
-
-    // search on main table if no symbol was found
-    auto it_obj = main_table_.lock()->Lookup(name);
-
-    if (it_obj != main_table_.lock()->end()) {
-      if (it_obj->second.global()) {
-      return std::tuple<std::shared_ptr<Object>,bool>(
-            it_obj->second.SharedAccess(), true);
-      }
-    }
-
-    return std::tuple<std::shared_ptr<Object>,bool>(
-          std::shared_ptr<Object>(nullptr), false);
-  }
-
-  bool Remove(const std::string& name) {
-    for (int i = (stack_.size() - 1); i >= 0 ; i--) {
-      bool r = stack_.at(i)->Remove(name);
-
-      if (r) {
-        return r;
-      }
-    }
-
-    return main_table_.lock()->Remove(name);
-  }
+  bool Remove(const std::string& name);
 
   bool InsertEntry(const std::string& name, SymbolAttr&& symbol) override {
     if (stack_.size() > 0) {
@@ -529,80 +415,19 @@ class SymbolTableStack: public SymbolTableStackBase {
     return false;
   }
 
-  std::vector<SymbolTablePtr> GetUntilFuncTable() const {
-    std::vector<SymbolTablePtr> statck;
+  std::vector<SymbolTablePtr> GetUntilFuncTable() const;
 
-    for (auto& table: stack_) {
-      if (table->Type() != SymbolTable::TableType::FUNC_TABLE) {
-        statck.push_back(table);
-        continue;
-      }
+  std::vector<SymbolTablePtr> GetUntilClassTable() const;
 
-      statck.push_back(table);
-
-      break;
-    }
-
-    return statck;
-  }
-
-  std::vector<SymbolTablePtr> GetUntilClassTable() const {
-    std::vector<SymbolTablePtr> statck;
-
-    for (auto& table: stack_) {
-      if (table->Type() != SymbolTable::TableType::CLASS_TABLE) {
-        statck.push_back(table);
-        continue;
-      }
-
-      statck.push_back(table);
-
-      break;
-    }
-
-    return statck;
-  }
-
-  void NewClassTable() {
+  inline void NewClassTable() {
      class_table_ = SymbolTablePtr(new SymbolTable);
   }
 
-  SymbolAttr& LookupClass(const std::string& name) {
-    // search on main table if no symbol was found
-    auto it_obj = class_table_->Lookup(name);
+  SymbolAttr& LookupClass(const std::string& name);
 
-    if (it_obj != class_table_->end()) {
-      return it_obj->second;
-    }
+  SymbolTablePtr& GetClassTable();
 
-    SymbolAttr& ref = class_table_->SetValue(name);
-    return ref;
-
-    throw RunTimeError(RunTimeError::ErrorCode::SYMBOL_NOT_FOUND,
-                       boost::format("symbol '%1%' not found")% name);
-  }
-
-  SymbolTablePtr& GetClassTable() {
-    for (auto& table: stack_) {
-      if (table->Type() == SymbolTable::TableType::CLASS_TABLE) {
-        return table;
-      }
-    }
-
-    throw RunTimeError(RunTimeError::ErrorCode::SYMBOL_NOT_FOUND,
-                       boost::format("can't find class symbol table"));
-  }
-
-  bool ExistsSymbolInClass(const std::string& name) {
-    // search on main table if no symbol was found
-    auto it_obj = class_table_->Lookup(name);
-
-    if (it_obj != class_table_->end()) {
-      return true;
-    }
-
-    return false;
-  }
+  bool ExistsSymbolInClass(const std::string& name);
 
   void Dump() override {
     std::cout << "*************\n";
